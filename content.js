5fc5fa37a8ad3bc361fd82e2460ce35cde6e4d12
jhinghaus <jhinghaus@gmail.com>
2019-11-19 23:42:37 +0100

    init

---
+++ content.js
@@ -0,0 +1,44 @@
+var stands = {"features":[
+  {"type": "Feature",
+   "geometry":
+    {
+		"type": "Point",
+		"coordinates": [8.0432, 52.2772]
+  },
+   "properties": {
+     "name": "Beispielstand 1",
+     "description": "Hier kann noch Text stehen",
+     "tags": ["Essen", "Burger", "Vegetatisch"],
+     "icon": "fa-cutlery"
+    }
+  },
+
+  {"type": "Feature",
+   "geometry":
+    {
+		"type": "Point",
+		"coordinates": [8.04324, 52.2773]
+  },
+   "properties": {
+     "name": "Beispielstand 2",
+     "description": "Hier kann noch Text stehen",
+     "tags": ["Spielzeug"],
+     "icon": "fa-child"
+    }
+  },
+
+  {"type": "Feature",
+   "geometry":
+    {
+    "type": "Point",
+    "coordinates": [8.043, 52.2774]
+  },
+   "properties": {
+     "name": "Beispielstand 3",
+     "description": "Hier gibt es Glühwein",
+     "tags": ["Getränke"],
+     "icon": "fa-coffee"
+    }
+  },
+]
+};
---
+++ main.js
@@ -0,0 +1,27 @@
+var map = L.map('map').setView([52.2772, 8.0432], 18);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// customize functions
+function createPopup(layer) {
+  return layer.feature.properties.name + "<br>" + layer.feature.properties.description;
+}
+
+function createMarker(geoJsonPoint, latlng) {
+  var icon = geoJsonPoint.properties.icon;
+  return L.marker(latlng, {
+  icon: L.divIcon({
+    html: '<i class="fa ' + icon + '"></i>',
+    iconSize: [20, 20],
+    className: 'StandIcon'
+  })
+});
+}
+
+// customize the stands
+var stand_geo = L.geoJSON(stands, {pointToLayer: createMarker}).bindPopup(createPopup);
+
+// add stands to the map
+var contentLayer = stand_geo.addTo(map);
